65f26ef26da1a80c7f3c9f2bc8cf2b115713cd4f
liuyuan <939692178@qq.com>
2019-07-08 22:43:12 +0800

    feat:add demo01 打包js

---
+++ demo01/app.js
@@ -0,0 +1,17 @@
+
+/**
+ * webpack支持ES6、CommonJs和AMD规范
+ */
+
+// ES6
+import sum from "./vendor/sum";
+console.log("sum(1, 2) = ", sum(1, 2));
+
+// CommonJs
+var minus = require("./vendor/minus");
+console.log("minus(1, 2) = ", minus(1, 2));
+
+// AMD => 1.xxx.js
+require(["./vendor/multi"], function(multi) {
+  console.log("multi(1, 2) = ", multi(1, 2));
+});
